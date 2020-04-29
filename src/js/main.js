9e2bfc29e4b830117f42696bf2f41e1f02b07c0a
budfy <serdyuk.ro@gmail.com>
2020-04-29 12:17:00 +0300

    Секция продуктов

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+/** @format */
 
+$(function () {
+	$(".product__tab-btn_js").on("click", function (e) {
+		let id = $(this).attr("href");
+		e.preventDefault();
+
+		$(this).addClass("product__tab-btn--current");
+		$(this)
+			.siblings(".product__tab-btn")
+			.removeClass("product__tab-btn--current");
+		$(id).addClass("product__tab--current");
+		$(id).siblings(".product__tab_js").removeClass("product__tab--current");
+	});
 });
