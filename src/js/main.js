c58d3ea12f7f40a55a85827ae125a2e050f96677
budfy <serdyuk.ro@gmail.com>
2020-05-05 11:35:57 +0300

    contact section added

--- src/js/main.js
+++ src/js/main.js
@@ -68,13 +68,35 @@ $(function () {
 		loop: false,
 		autoHeight: false,
 		slidesPerGroup: 1,
-		pagination: {
-			el: ".js-swiper__galery .swiper-pagination",
-			clickable: true,
-		},
 		navigation: {
 			nextEl: ".js-swiper__galery .swiper-button-next",
 			prevEl: ".js-swiper__galery .swiper-button-prev",
+		},
+		breakpoints: {
+			990: {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				spaceBetween: 16,
+			},
+			640: {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				slidesPerGroup: 1,
+				spaceBetween: 16,
+			},
+		},
+	});
+
+	var review_carusel = new Swiper(".js-swiper__awards .swiper-container", {
+		slidesPerView: 5,
+		slidesPerColumn: 1,
+		spaceBetween: 16,
+		loop: false,
+		autoHeight: false,
+		slidesPerGroup: 1,
+		navigation: {
+			nextEl: ".js-swiper__awards .swiper-button-next",
+			prevEl: ".js-swiper__awards .swiper-button-prev",
 		},
 		breakpoints: {
 			990: {
@@ -131,5 +153,30 @@ $(function () {
 			$('#' + this__link + ' .js-title--val').text(title);
 			$('#' + this__link + ' .js-value--val').text(value);
 		});
+	});
+
+	$(".questions-item").click(function () {
+		$(this).toggleClass("questions-item--closed");
+		$(this).children(".questions-inner").slideToggle(500);
+		$(this).siblings().children(".questions-inner").slideUp(500);
+		$(this).siblings().addClass("questions-item--closed");
+	});
+
+	$(document).mouseup(function (e) {
+		var container = $(".questions-item");
+		if (container.has(e.target).length === 0) {
+			container.addClass("questions-item--closed");
+			container.children(".questions-inner").slideUp(500);
+		}
+	});
+
+	$('a[href*="#"]').on('click', function (e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+
+		$('body,html').animate({
+			scrollTop: (top - 150)
+		}, 1500);
 	});
 });
